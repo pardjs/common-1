1cbef2219ca23379cf0ae793fcdece43e79716ca
Bai Guodong <bgd_sh@hotmail.com>
2019-04-24 14:17:06 +0800

    feat(): add sha1, password, base64 utils

---
+++ src/utils/encrypt.helpers.spec.ts
@@ -0,0 +1,10 @@
+import { comparePassword, hashPassword } from "./encrypt.helpers";
+
+describe("password hash and compare", () => {
+  it("works", async () => {
+    const raw = "password";
+    const hashed = await hashPassword(raw);
+    const compareResult = await comparePassword(raw, hashed);
+    expect(compareResult).toBeTruthy();
+  });
+});
--- src/utils/encrypt.helpers.ts
+++ src/utils/encrypt.helpers.ts
@@ -1,4 +1,6 @@
-import { createHash, createHmac } from "crypto";
+import * as bcrypt from "bcrypt";
+import { createHash, createHmac, HexBase64Latin1Encoding } from "crypto";
+
 export const md5 = (raw: string) => {
   return createHash("md5")
     .update(raw)
@@ -14,4 +16,28 @@ export const sha256 = (raw: string, key: string) => {
 export const superMd5 = (raw: string, key: string) => {
   const hashed = sha256(raw, key);
   return md5(hashed);
+};
+
+export const hashPassword = async (raw: string, saltRound: number = 10) => {
+  const hash = await bcrypt.hash(raw, saltRound);
+  return hash;
+};
+
+export const comparePassword = async (raw: string, hash: string) => {
+  const result = await bcrypt.compare(raw, hash);
+  return result;
+};
+
+export const sha1 = (
+  key: string,
+  content: string,
+  encoding: HexBase64Latin1Encoding = "base64",
+) => {
+  return createHmac("sha1", key)
+    .update(content)
+    .digest(encoding);
+};
+
+export const objToBase64 = (obj: object) => {
+  return Buffer.from(JSON.stringify(obj)).toString("base64");
 };
